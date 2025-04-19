be51a83f4a4fd7ab4c73834bec7e71e87fb4b3d1
CrazyBotzz16 <stokggk106@gmail.com>
2025-04-19 15:40:04 +0700

    Update panels.js

--- panels.js
+++ panels.js
@@ -8,7 +8,11 @@
     {
       "username": "Roxxie",
       "password": "Crazybotzzz",
-      "ip_whitelist": ["140.213.231.15"]
+      "ip_whitelist": ["140.213.231.15",
+"140.213.64.225",
+"",
+""
+]
     }
   ]
 }
