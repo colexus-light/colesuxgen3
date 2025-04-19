63dc666abe25b8bd3bdaf49d28b59f0eb9c81aa4
CrazyBotzz16 <stokggk106@gmail.com>
2025-04-19 15:13:53 +0700

    Create panels.js

---
+++ panels.js
@@ -0,0 +1,14 @@
+{
+  "panels": [
+    {
+      "username": "ArdBotzz",
+      "password": "tybro",
+      "ip_whitelist": ["140.213.231.15"]
+    },
+    {
+      "username": "Roxxie",
+      "password": "Crazybotzzz",
+      "ip_whitelist": ["140.213.231.15"]
+    }
+  ]
+}
